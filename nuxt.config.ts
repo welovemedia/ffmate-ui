5c6bb0644e755101c15bb82cf9e32a092fde409f
Yo Sev <severinsiebertz@googlemail.com>
2025-02-07 14:10:10 +0100

    feat: upgrade to tailwind4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@vueuse/nuxt", "nuxt-umami"],
+  modules: [
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    "nuxt-umami",
+    "@nuxtjs/google-fonts",
+  ],
   ssr: false,
   css: ["~/assets/css/style.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   app: {
     pageTransition: { name: "page", mode: "out-in" },
     baseURL: "/ui/",
@@ -21,6 +30,13 @@ export default defineNuxtConfig({
       title: "ffmate",
       htmlAttrs: {
         class: "overflow-y-scroll",
+      },
+    },
+  },
+  googleFonts: {
+    families: {
+      Poppins: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
       },
     },
   },
